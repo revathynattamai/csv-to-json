577c955ea4f9af3b9f158603a041c14020b2add7
revathynattamai <revathy.ng@gmail.com>
2017-05-07 22:47:23 +0530

    Added parser

--- indicators.js
+++ indicators.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const stream = require('stream');
+require("colors");
 
 const inputFile = "indicators.csv";
 const outputFile = "indicators.json"
@@ -20,11 +21,52 @@ var writeDataToFile = function(data, type) {
 };
 //Writes the initial data
 writeDataToFile('{\n\"data\":[\n');
+startTime = new Date().getTime();
+console.log(('Started Parsing at '+startTime+' ms.').green);
+
+
+
+rl.on('line', function(data) {
+    parser(data);
+});
 
 //Appends the final data
 rl.on('close', function() {
     writeDataToFile('\n\t]\n}', 1);
+    endTime = new Date().getTime();
+    console.log(('Finished Parsing at '+endTime+' ms.').green);
+    console.log(("Execution time = " + ((endTime - startTime)/1000)  +' sec' ).green);
 });
 
+var columns = [];
+
+var parser = function(line) {
+    if(i == 1) {
+        columns = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+        // console.log(columns);
+    }
+    else {
+        var newLine = [];
+        lineTemp = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+        lineTemp.forEach(function(string) {
+            newLine.push(string.replace(/['"]+/g, ''));
+        });
+        var fields = ["Life expectancy at birth, female (years)", "Life expectancy at birth, male (years)", "Birth rate, crude (per 1,000 people)", "Death rate, crude (per 1,000 people)"];
+        if(fields.indexOf(newLine[2]) !== -1) {
+            var newObj = {};
+            newObj[columns[0]] = newLine[0];
+            newObj[columns[1]] = newLine[1];
+            newObj[columns[2]] = newLine[2];
+            newObj[columns[3]] = newLine[3];
+            newObj[columns[4]] = newLine[4];
+            newObj[columns[5]] = newLine[5];
+            var data = JSON.stringify(newObj);
+            if (data != '{}') {
+                writeDataToFile(data + ",\n", 1);
+            }
+        }
+    }
+    i++;
+}
 
 
